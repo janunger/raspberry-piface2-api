2140e36a4d54a2e9a7e1b10676c15353ec8c14dc
Jan Unger <jan.unger@juit.de>
2017-10-25 15:06:41 +0200

    Removing single input pin representation

--- src/Hardware/OutputPin.php
+++ src/Hardware/OutputPin.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace JUIT\PiFace\Hardware;
 
-class OutputPin extends Pin implements \JUIT\PiFace\OutputPin
+class OutputPin implements \JUIT\PiFace\OutputPin
 {
+    /** @var int */
+    private $id;
+
+    /** @var ProcessRunner */
+    private $processRunner;
+
+    public function __construct(int $id, ProcessRunner $processRunner = null)
+    {
+        $this->id = $id;
+
+        if (!$processRunner) {
+            $processRunner = new ProcessRunner();
+        }
+        $this->processRunner = $processRunner;
+    }
+
     public function trigger(int $durationMilliseconds)
     {
         $command = sprintf('python3 %s/bin/trigger.py %s %s', __DIR__, $this->id, $durationMilliseconds);
