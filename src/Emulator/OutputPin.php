fe10179e1d9dc580dc7eb2850cd277e52f7f8b23
Jan Unger <jan.unger@juit.de>
2017-11-01 21:13:20 +0100

    Emulator output pin can be switched on and off

--- src/Emulator/OutputPin.php
+++ src/Emulator/OutputPin.php
@@ -6,6 +6,18 @@ namespace JUIT\PiFace\Emulator;
 
 class OutputPin implements \JUIT\PiFace\OutputPin
 {
+    /**
+     * @var \SplFileInfo
+     */
+    private $dataFile;
+
+    private $isOn = false;
+
+    public function __construct(\SplFileInfo $dataFile)
+    {
+        $this->dataFile = $dataFile;
+    }
+
     public function trigger(int $durationMilliseconds)
     {
         usleep($durationMilliseconds * 1000);
@@ -13,9 +25,16 @@ class OutputPin implements \JUIT\PiFace\OutputPin
 
     public function switchOn()
     {
+        $this->isOn = true;
     }
 
     public function switchOff()
     {
+        $this->isOn = false;
+    }
+
+    public function isOn(): bool
+    {
+        return $this->isOn;
     }
 }
---
+++ tests/Emulator/OutputPinTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JUIT\PiFace\Tests\Emulator;
+
+use JUIT\PiFace\Emulator\OutputPin;
+use PHPUnit\Framework\TestCase;
+
+class OutputPinTest extends TestCase
+{
+    /** @var \SplFileInfo */
+    private $dataFile;
+
+    /** @var OutputPin */
+    private $SUT;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->dataFile = new \SplFileInfo(PROJECT_ROOT_DIR . '/var/emulator/output_pin');
+        $this->SUT = new OutputPin($this->dataFile);
+        $this->clearDataFile();
+    }
+
+    /** @test */
+    public function it_can_be_switched_on_and_off()
+    {
+        static::assertFalse($this->SUT->isOn());
+
+        $this->SUT->switchOn();
+        static::assertTrue($this->SUT->isOn());
+
+        $this->SUT->switchOff();
+        static::assertFalse($this->SUT->isOn());
+    }
+
+    protected function tearDown()
+    {
+        $this->clearDataFile();
+
+        parent::tearDown();
+    }
+
+    private function clearDataFile()
+    {
+        if (!$this->dataFile->isFile()) {
+            return;
+        }
+        unlink($this->dataFile->getPathname());
+    }
+}
